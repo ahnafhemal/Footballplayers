2ec18aae79f0fc80ea116c53430f65a1dfdcc832
ahnafhemal <hemalhasan140@gmail.com>
2023-03-16 22:40:25 +0600

    display data

--- js/script.js
+++ js/script.js
@@ -4,10 +4,32 @@ const allPlayers = () => {
 
     fetch(url)
         .then(res => res.json())
-        .then(data => displayAllPlayer(data))
+        .then(data => showPlayerDetails(data.player))
 }
 
 
-const displayAllPlayer = (players) => {
+const showPlayerDetails = (players) => {
+    const perent = document.getElementById('player-container')
 
+
+
+    for (const player of players) {
+        console.log(player)
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <div class="card border">
+<div class="pro-pic">
+    <img class="w-25" src="" alt="">
+</div>
+<h3>Name:</h3>
+<h5>Country:</h5>
+<p></p>
+<div class="allbutton">
+    <button class="btn btn-danger">Delete</button>
+    <button class="btn btn-primary">Details</button>
+</div>
+</div>
+        `
+        perent.appendChild(div)
+    }
 }
