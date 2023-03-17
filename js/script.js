bdc42427e42df52122b9aff109c88b48fc947dcb
ahnafhemal <hemalhasan140@gmail.com>
2023-03-17 07:02:49 +0600

    display players

--- js/script.js
+++ js/script.js
@@ -17,19 +17,27 @@ const showPlayerDetails = (players) => {
         console.log(player)
         const div = document.createElement('div')
         div.innerHTML = `
-        <div class="card border">
+        <div class="card border m-4 p-3">
 <div class="pro-pic">
-    <img class="w-25" src="" alt="">
+    <img class="w-50" src="${player.strThumb}" alt="">
 </div>
-<h3>Name:</h3>
-<h5>Country:</h5>
+<h3>Name: ${player.strPlayer}</h3>
+<h5>Country: ${player.strNationality}</h5>
 <p></p>
 <div class="allbutton">
     <button class="btn btn-danger">Delete</button>
-    <button class="btn btn-primary">Details</button>
+    <button onClick="details(${player.idPlayer})" class="btn btn-primary">Details</button>
 </div>
 </div>
         `
         perent.appendChild(div)
     }
+}
+
+
+const details = (idPlayer) => {
+    const url = `https://www.thesportsdb.com/api/v1/json/3/lookupplayer.php?id=${idPlayer}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
 }
